72bdc94af9256e3e36c8b18c54227f3a47370ebe
Artem Voronov <voronov_artem_lit@mail.ru>
2025-05-28 21:31:06 +0300

    param: adjust carpole physics parameters

--- Project 5 HardPole/cartpole.py
+++ Project 5 HardPole/cartpole.py
@@ -7,7 +7,9 @@ class CartPoleParams:
     m_pole: float = 0.1      # mass of the pole
     l: float = 0.5           # half-length of the pole
     g: float = 9.81          # gravity
-    damping: float = 0.01    # simple friction coefficient
+    damping: float = 30    # simple friction coefficient
+    rotary_damping: float = 0.3  # pole angular damping
+    max_force: float = 50.0
 
 class CartPole:
     def __init__(self, init_state: np.ndarray, params: CartPoleParams = None):
@@ -30,12 +32,16 @@ class CartPole:
         pole_mass_length = p.m_pole * p.l
 
         temp = (force + pole_mass_length * theta_dot**2 * sin_theta - p.damping * x_dot) / total_mass
-        theta_acc = (p.g * sin_theta - cos_theta * temp) / (p.l * (4/3 - p.m_pole * cos_theta**2 / total_mass))
+
+        theta_acc = (p.g * sin_theta - cos_theta * temp - p.rotary_damping * theta_dot) / \
+                    (p.l * (4/3 - p.m_pole * cos_theta**2 / total_mass))
+
         x_acc = temp - pole_mass_length * theta_acc * cos_theta / total_mass
 
         return np.array([x_dot, x_acc, theta_dot, theta_acc])
 
     def update(self, force: float, dt: float):
+        force = np.clip(force, -self.params.max_force, self.params.max_force)
         derivs = self.dynamics(self.state, force)
         self.state += derivs * dt
         self.state[2] = (self.state[2] + np.pi) % (2 * np.pi) - np.pi  # wrap theta ∈ [-π, π]
